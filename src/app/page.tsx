ea00d9048555e4cdb12449e93ddd463545985fc6
Erban Danaj <danajerban@gmail.com>
2024-10-13 03:02:48 +0200

    clerk setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,46 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { auth, currentUser, User } from "@clerk/nextjs/server";
+import getUsers from "./get-users";
 
-export default function Home() {
+export default async function Home() {
+  const authDetails = auth();
+  console.log("authDetails", authDetails);
+
+  const user = await currentUser();
+  console.log("user", user);
+
+  const allUsers = await getUsers();
+  console.log("allUsers", allUsers);
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="h-screen flex items-center justify-center flex-col gap-5">
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
         <UserButton />
+        <SignOutButton />
       </SignedIn>
+      <br></br>
+      Current User - {user?.firstName} {user?.lastName}
+      <br></br>
+      <br></br>
+      <div>
+        <h1>All Users</h1>
+        <ol>
+          {allUsers.data?.map((_user: User) => (
+            <li key={_user.id}>
+              {_user.firstName} {_user.lastName}
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
